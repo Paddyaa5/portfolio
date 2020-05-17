223933a408ca868fabb59b61327a86b0239b44a4
Paddyaa5 <paddyaa5@gmail.com>
2020-05-17 20:11:36 +0100

    navbar re-sizing complete - Added new menu animations

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const drop = () => {
-    
+const navSlide = () => {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.links');
+    const links = document.querySelectorAll('.links li');
+
+    menu.addEventListener('click', () => {
+        nav.classList.toggle('linksActive')
+        links.forEach ((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `linkFade 0.5s ease forwards ${index / 7+.5}s`;
+            }
+        });
+        menu.classList.toggle('exit');
+    });
 }
+navSlide();
